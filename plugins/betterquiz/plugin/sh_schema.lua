e3383f96d304ae56ade69b4464866317b0d00d8f
Allen <cdolson30@gmail.com>
2015-07-24 13:43:28 -0400

    new file:   plugins/betterquiz/plugin.ini 	new file:   plugins/betterquiz/plugin/sh_plugin.lua 	new file:   plugins/betterquiz/plugin/sh_schema.lua 	new file:   plugins/mpfdispatchvoices/plugin.ini 	new file:   plugins/mpfdispatchvoices/plugin/libraries/sh_voices.lua

---
+++ plugins/betterquiz/plugin/sh_schema.lua
@@ -0,0 +1,16 @@
+Clockwork.quiz:SetEnabled(true);
+Clockwork.quiz:AddQuestion("Do you understand that roleplaying is slow paced and relaxed?", 1, "Yes.", "No.");
+Clockwork.quiz:AddQuestion("Can you type properly, using capital letters and punctuation?", 2, "yes i can", "Yes, I can.");
+Clockwork.quiz:AddQuestion("You do not need weapons to roleplay. Is this correct?", 1, "Yes.", "No.");
+Clockwork.quiz:AddQuestion("You do not need items to roleplay. Is this correct?", 1, "Yes.", "No.");
+Clockwork.quiz:AddQuestion("What do you think serious roleplaying is about?", 2, "Collecting items and upgrades.", "Developing your character.");
+Clockwork.quiz:AddQuestion("What universe is this roleplaying game set in?", 2, "Real Life.", "Half-Life 2.");
+Clockwork.quiz:AddQuestion("What do you roleplay as by default in HL2RP?", 1, "Citizen.", "Metropolice Force.");
+Clockwork.quiz:AddQuestion("What pace is CW: HL2RP?", 2, "All about action and guns.", "Slow-paced and developed roleplay.");
+Clockwork.quiz:AddQuestion("Who created this schema? (Go back if you don't know.)", 1, "Kurozael and Alex Grist.", "Chessnut");
+
+-- Want to edit the questions or make your own? Use this format:
+-- Clockwork.quiz:AddQuestion("< Insert Question >", < Correct Choice, 1 or 2 >, "< Choice 1 >", "< Choice 2 >");
+
+-- Example:
+-- Clockwork.quiz:AddQuestion("Is this a question?", 2, "No", "Yes");
